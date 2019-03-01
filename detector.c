464bf31731a5b9e985777a9d859f6eae993b3603
maryam <ismailova.maryam@gmail.com>
2019-03-01 16:24:38 +0400

    detector time

--- detector.c
+++ detector.c
@@ -4,11 +4,18 @@
 #include <stdbool.h>
 #include <unistd.h>
 #include <getopt.h>
+#include <time.h>
 
 bool printReturnCode=false;
-int interval=0;
+bool timeFormatSet=false;
+bool limitSet=false;
+bool intervalSet=false;
+
+int interval=10000;//in millis
 char timeFormat[100];
 int limitOfIterations=0;//0 for infinity
+
+
 
 int main(int argc, char *argv[])
 {
@@ -26,13 +33,16 @@ int main(int argc, char *argv[])
                 break;
             case 'i':
                 interval=atoi(argv[optind-1]);
+                intervalSet=true;
                 printf("Set interval to %d\n", interval);
                 break;
             case 't':
+                timeFormatSet=true;
                 strcpy(timeFormat, argv[optind-1]);
                 printf("Set time format to %s\n", timeFormat);
                 break;
             case 'l':
+                limitSet=true;
                 limitOfIterations=atoi(argv[optind-1]);
                 printf("Set limit to %d\n", limitOfIterations);
                 break;
@@ -46,10 +56,17 @@ int main(int argc, char *argv[])
     }
     // printf("optind %d\n", optind);
     progStartIndex=optind;
-    
     int counter=0;
-    while(counter<limitOfIterations){
-
+    while(counter<limitOfIterations || limitSet==false){
+        if(timeFormatSet){
+            char currentTime[100];
+            time_t curTime=time(NULL);
+            struct tm* locTime=localtime(&curTime);
+            strftime(currentTime, 100, timeFormat, locTime);
+            printf("currentTime - %s\n", currentTime);
+        }
+        usleep(interval*1000);
+        counter++;
     }
     return 0;
 }
