692bb20875adb54131ed5ac83d19ab4fb4dea9ea
maryam <ismailova.maryam@gmail.com>
2019-03-11 14:40:39 +0400

    work

--- detector.c
+++ detector.c
@@ -6,8 +6,10 @@
 #include <getopt.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <sys/mman.h>
 #include <time.h>
 
+#define MAXSIZE 4096
 bool printReturnCode=false;
 bool timeFormatSet=false;
 bool limitSet=false;
@@ -16,91 +18,144 @@ bool intervalSet=false;
 int interval=10000;//in millis
 char timeFormat[100];
 int limitOfIterations=0;//0 for infinity
+char *previousOutput;
 
 int runCommand(char *command);
 
 int runCommand(char *command){
+    int outputSize=MAXSIZE+1;
+    char *outputString=calloc(outputSize, sizeof(char));
+    if(outputString==NULL){
+        perror("calloc");
+        exit(0);
+    }
+    memset(outputString, 0, MAXSIZE);
+    int pipes[2];
+    if(pipe(pipes)==-1){
+        perror("Pipe");
+        exit(0);
+    }
     int pid=fork();
     if(pid==0){
-        printf("run command\n");
+        dup2(pipes[1], 1);
+        close(pipes[0]);
+        close(pipes[1]);
         execlp("/bin/sh","sh", "-c", command, NULL);
-        // execvp(, command);
-        // printf("run \"%s\"\n", command);
-        // execvp()
-        // exit(3);
     }else if(pid==-1){
-        exit(-1);
+        perror("Fork");
+        exit(0);
     }else
-    {
+    {   close(pipes[1]);
+        char *readChar=calloc(1024+1, sizeof(char));
+        if(readChar==NULL){
+            perror("Calloc");
+            exit(0);
+        }
+        int nbBytes=0;
+        int nbRead=0;
+        while((nbBytes=read(pipes[0], readChar, 1024))>0){
+            nbRead+=nbBytes;
+            if(nbRead>outputSize){
+                outputSize=nbRead+1;
+                outputString=realloc(outputString, outputSize);
+                if(outputString==NULL){
+                    perror("Realloc");
+                    exit(0);
+                }
+            }
+            strcat(outputString, readChar);
+            memset(readChar, 0, 1025);
+        }
+
         int status;
         wait(&status);
         if(WIFEXITED(status)){
-            printf("child exits with status %d\n", WEXITSTATUS(status));
-            return WEXITSTATUS(status);
+            if(strcmp(previousOutput, outputString)==0){
+            }else{
+                previousOutput=calloc(outputSize, sizeof(char));
+                if(previousOutput==NULL){
+                    perror("Calloc");
+                    exit(0);
+                }
+                strcpy(previousOutput, outputString);
+                printf("%s\n", outputString);
+                // printf("exit %d\n", WEXITSTATUS(status));
+            }
         }
-    }
-    
+        return WEXITSTATUS(status);
+    }   
 }
 int main(int argc, char *argv[])
 {
-    fclose(stderr);
     int progStartIndex=-1;
     int opt=-1;
-    // dub2(, 2);
     char **argv2=calloc(argc, sizeof(char *));
+    if(argv2==NULL){
+        perror("Calloc");
+        exit(0);
+    }
     for(int i=0;i<argc;i++){
         argv2[i]=calloc(100, sizeof(char));
+        if(argv2[i]==NULL){
+            perror("Calloc");
+            exit(0);
+        }
         strcpy(argv2[i], argv[i]);
     }
     while((opt=getopt(argc, argv, "+ci:t:l:"))!=-1){
-    // printf("in while\n");
         switch (opt)
         {
             case 'c':
                 if(printReturnCode==false){
                     printReturnCode=true;
-                    printf("print Return code true\n");
+                    // printf("Return code true\n");
                 }
                 break;
             case 'i':
                 if(intervalSet==false){
-                    interval=atoi(argv[optind-1]);
+                    // interval=atoi(argv[optind-1]);
+                    interval=atoi(optarg);
                     intervalSet=true;
-                    printf("Set interval to %d\n", interval);
+                    // printf("Set interval to %d\n", interval);
                 }
                 break;
             case 't':
                 if(timeFormatSet==false){
                     timeFormatSet=true;
-                    strcpy(timeFormat, argv[optind-1]);
-                    printf("Set time format to %s\n", timeFormat);
+                    // strcpy(timeFormat, argv[optind-1]);
+                    strcpy(timeFormat, optarg);
+                    // printf("Set time format to %s\n", timeFormat);
                 }
                 break;
             case 'l':
             if(limitSet==false){
                 limitSet=true;
-                limitOfIterations=atoi(argv[optind-1]);
-                printf("Set limit to %d\n", limitOfIterations);
+                // limitOfIterations=atoi(argv[opind-1]);
+                limitOfIterations=atoi(optarg);
+                // printf("Set limit to %d\n", limitOfIterations);
             }
                 break;
             case '?':
-                printf("Option is needed\n");
+                // printf("Option is needed\n");
+                exit(0);
                 break;            
             default:
-                printf("default\n");
+                // printf("default\n");
                 break;
         }
     }
-    // printf("optind %d\n", optind);
     progStartIndex=optind;
     int length=argc-progStartIndex;
     char command[256];
     for(int i=0;i<length;i++){
         sprintf(command, "%s %s", command, argv2[i+progStartIndex]);
     }
-    printf("command: %s\n", command);
 
-    // progStartIndex=optind;
+    previousOutput=calloc(MAXSIZE+1, sizeof(char));
+    if(previousOutput==NULL){
+        perror("Calloc");
+        exit(0);
+    }
     int counter=0;
     while(counter<limitOfIterations || limitSet==false){
         if(timeFormatSet){
@@ -108,9 +163,10 @@ int main(int argc, char *argv[])
             time_t curTime=time(NULL);
             struct tm* locTime=localtime(&curTime);
             strftime(currentTime, 100, timeFormat, locTime);
-            printf("currentTime - %s\n", currentTime);
+            printf("%s\n", currentTime);
         }
-        runCommand(command);
+        int exitStatus=runCommand(command);
+        if(printReturnCode==true) printf("exit %d\n", exitStatus);
         usleep(interval*1000);
         counter++;
     }
